96ad1aa0d5e60e505b7f76c4153b39a9ab2746c4
Peixe3d <mateus.b.s.terra@gmail.com>
2016-12-06 23:31:39 -0200

    Copyright Notice Starfruit

--- Br++/src/main/java/br/com/RatosDePC/Brpp/BrppCompilerMain.java
+++ Br++/src/main/java/br/com/RatosDePC/Brpp/BrppCompilerMain.java
@@ -1,12 +1,33 @@
 package br.com.RatosDePC.Brpp;
 
+/*
+Copyright (c) 2016 StarFruitBrasil
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 /**
  * 
  * Classe principal do programa
  * 
  * @author Mateus Berardo de Souza Terra
  * @contributors 
- * @version 5/2/2016
+ * @version 18/11/2016
  */
 
 import java.io.File;
@@ -42,17 +63,15 @@ public class BrppCompilerMain {
 			e.printStackTrace();
 		}
 		try {
-			
-			FileUtils.copyFolder(l,destDir);
+			FileUtils.copyFolder(l, destDir);
 			KeywordManagerUtils.processLibraries();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (NullPointerException e){
+		} catch (NullPointerException e) {
 			e.printStackTrace();
 		}
-		BrppIDEFrame frame = new BrppIDEFrame("Brino "
-				+ BrppCompiler.version);
+		BrppIDEFrame frame = new BrppIDEFrame("Brino " + BrppCompiler.version);
 		frame.setSize(500, 600);
 		frame.setVisible(true);
 		frame.setLocation(100, 30);
